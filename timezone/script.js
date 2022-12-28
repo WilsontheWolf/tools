c15c4f0cb53594053a56c1b485829c47212ba116
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2022-12-28 13:12:22 -0700

    chore: improvements

--- timezone/script.js
+++ timezone/script.js
@@ -1,9 +1,12 @@
+let toCopy;
+
 window.fetchTimezone = () => {
     const output = document.getElementById('timezone');
     
     window.fetch('https://worldtimeapi.org/api/ip')
         .then(response => response.json())
         .then(data => {
+    		toCopy = data.timezone;
             output.innerHTML = `<code>${data.timezone}</code> <a href="javascript:alert('Fetched from worldtimeapi.org for ip ${data.client_ip}');">i</a>` || 'Timezone not found.';
         });
 }
@@ -15,6 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (!timezone) {
         output.innerHTML = 'Timezone not found. Consider clicking the button below to fetch it.';
     } else {
+    	toCopy = timezone;
         output.innerHTML = `<code>${timezone}</code>`;
 
         if (!timezone.includes('/') || timezone.startsWith('Etc/')) {
@@ -22,4 +26,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+	const copy = document.getElementById('copy');
+	copy.addEventListener('click', () => {
+		navigator.clipboard.writeText(toCopy);
+	})
 });
